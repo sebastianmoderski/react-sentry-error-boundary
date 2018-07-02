0176b4b067ea44c7dde78ffe258433d2f346a54c
Sebastian Moderski <s.moderski@gmail.com>
2018-07-01 22:28:28 -0600

    Initial setup; Create react sentry error boundary component

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # react-sentry-error-boundary
-A React error boundary component with integrated Sentry error reporting
+A React error boundary component with integrated Sentry error reporting using Raven JS.
+
+### Installation
+
+```
+npm install --save react-sentry-error-boundary
+```
+
+### Requirements
+
+```
+raven-js >= 3.36.3
+react >= 16.0.0
+```
+
+### Usage
+
+```js
+import MyWidget from './MyWidget'
+import SentryErrorBoundary from 'react-sentry-error-boundary'
+import React from 'react'
+
+export default const App = () => {
+  <SentryErrorBoundary dsn="https://<key>@sentry.io/<project>">
+    <MyWidget />
+  </SentryErrorBoundary>
+}
+```
+
+### Props
+
+**config**: Sentry configuration object ([info](https://docs.sentry.io/clients/node/config/))
+
+**dsn (required)**: Sentry DSN URL ([info](https://docs.sentry.io/quickstart/#configure-the-dsn))
+
+**errorNode**: A node to render on error
+
+**userContext**: Sentry user context object ([info](https://docs.sentry.io/learn/context/))
---
+++ src/ReactSentryErrorBoundary.js
@@ -0,0 +1,79 @@
+import Raven from '../node_modules/raven-js/src/singleton'
+import React, {
+  PropTypes
+} from 'react'
+
+export default class ReactSentryErrorBoundary extends React.Component {
+  static propTypes = {
+    children: PropTypes.node.isRequired,
+    config: PropTypes.object,
+    dsn: PropTypes.string.isRequired,
+    errorNode: PropTypes.node,
+    userContext: PropTypes.object
+  }
+
+  static defaultProps = {
+    config: {}
+  }
+
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      hasError: false
+    }
+  }
+
+  componentDidCatch (error) {
+    this.setState({
+      hasError: true
+    })
+
+    if (!this.ravenClient) {
+      this.ravenClient = new Raven.Client()
+
+      this.configureRaven(
+        this.props.dsn,
+        this.props.config,
+        this.props.userContext
+      )
+    }
+
+    this.logError(error)
+  }
+
+  componentWillRecieveProps (nextProps) {
+    if (!this.ravenClient) return
+
+    const isChangedConfig = JSON.stringify(this.nextProps.config) !== JSON.stringify(this.props.config)
+    const isChangedDsn = this.nextProps.dsn !== this.props.dsn
+    const isChangedUserContext = JSON.stringify(this.nextProps.userContext) !== JSON.stringify(this.props.userContext)
+
+    if (!isChangedConfig && !isChangedDsn && !isChangedUserContext) return
+
+    this.configureRaven(
+      this.nextProps.dsn,
+      this.nextProps.config,
+      this.nextProps.userContext
+    )
+  }
+
+  configureRaven (dsn, config, userContext) {
+    this.ravenClient.config(
+      dsn,
+      config
+    )
+
+    if (userContext) this.ravenClient.setUserContext(userContext)
+  }
+
+  logError (error) {
+    this.ravenClient.captureException(error)
+  }
+
+  render () {
+    if (this.state.hasError && this.props.errorNode) return this.props.errorNode
+
+    return this.props.children
+  }
+}
